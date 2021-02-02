4998fd81a1ef89ec7eb2b18ff0ac32ae1531bbd6
Oliver Steiner <oliver@mollo.ch>
2021-02-02 02:38:53 +0100

    list all 'dashboard_' Views

--- src/Controller/MolloDashboardController.php
+++ src/Controller/MolloDashboardController.php
@@ -5,6 +5,7 @@ namespace Drupal\mollo_dashboard\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\mollo_drupal\Utils\MolloDashboardTrait;
 use Drupal\mollo_module\Utils\MolloModuleTrait;
+use Drupal\views\Views;
 
 
 /**
@@ -28,6 +29,14 @@ class MolloDashboardController extends ControllerBase {
 
   public $bar;
 
+  public $views;
+
+  public $name;
+
+  public $title;
+
+  public $icon;
+
 
   /**
    * @return array[]
@@ -43,7 +52,7 @@ class MolloDashboardController extends ControllerBase {
       'description' => [
         '#type' => 'inline_template',
         '#template' => $template,
-        '#context' => $this->getPageVars(),
+        '#context' => $this->getViewsData(),
       ],
     ];
   }
@@ -62,6 +71,80 @@ class MolloDashboardController extends ControllerBase {
     $variables['mollo_dashboard']['test'] = $test;
     $variables['mollo_dashboard']['foo'] = $foo;
     $variables['mollo_dashboard']['bar'] = $bar;
+
+    return $variables;
+  }
+
+  /**
+   * @return array
+   *
+   */
+  private function getViewsData(): array {
+
+    $views_demo = [
+      [
+        'name' => 'dashboard_last_edit',
+        'title' => 'Last Changes',
+        'icon' => 'fal fa-clock',
+      ],
+      [
+        'name' => 'dashboard_unpublished',
+        'title' => 'Unpublished',
+        'icon' => 'fal fa-eye-slash',
+      ],
+      [
+        'name' => 'dashboard_intern',
+        'title' => 'Intern',
+        'icon' => 'fal fa-lock',
+      ],
+      [
+        'name' => 'dashboard_blog',
+        'title' => 'Blog',
+        'icon' => 'fal fa-typewriter',
+      ],
+      [
+        'name' => 'dashboard_pages',
+        'title' => 'Basic Pages',
+        'icon' => 'fal fa-file',
+      ],
+    ];
+
+    $views_icons = [
+      'dashboard_last_edit' => 'fal fa-clock',
+      'dashboard_unpublished' => 'fal fa-eye-slash',
+      'dashboard_intern' => 'fal fa-lock',
+      'dashboard_blog' => 'fal fa-typewriter',
+      'dashboard_pages' => 'fal fa-file',
+    ];
+
+    $views = [];
+
+    // Get List with all View Ids
+    $views_on_site = Views::getViewsAsOptions(TRUE, 'enabled');
+
+    foreach ($views_on_site as $view_name => $view_title) {
+
+      // take only View with 'dashboard_' in name
+      $needle = 'dashboard_';
+      if (strpos($view_name, $needle) !== FALSE) {
+
+        // Title: Remove 'Dashboard' from Title
+        $title = str_replace('Dashboard', '', $view_title);
+
+        // Icon: Search in Predefined List
+        $icon = $views_icons[$view_name] ?? '';
+
+        $view['name'] = $view_name;
+        $view['title'] = $title;
+        $view['icon'] = $icon;
+        $views[] = $view;
+      }
+    }
+    //
+
+    // dd($views);
+
+    $variables['mollo_dashboard']['views'] = $views;
 
     return $variables;
   }
